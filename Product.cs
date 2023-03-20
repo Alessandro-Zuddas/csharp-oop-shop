0f3283ddec1848b0fc70e0675b7b5f31eb4e9854
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-20 16:44:56 +0100

    Add milestone di base

---
+++ Product.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Product
+    {
+        int productCode;
+        string productName;
+        string productDescription;
+        int productPrice;
+        int iva = 22;
+
+        public Product(string name, string description, int price)
+        {
+            if (productCode == 0)
+            {
+                Random rnd = new Random();
+                productCode = rnd.Next();
+            }
+
+            productName = name;
+            productDescription = description;
+            productPrice = price;
+        }
+
+        public int getCode()
+        {
+            return this.productCode;
+        }
+
+        public string getName()
+        {
+            return this.productName;
+        }
+
+        public string setName(string value)
+        {
+            return this.productName = value;
+        }
+
+        public string getDescription()
+        {
+            return this.productDescription;
+        }
+
+        public string setDescription(string value)
+        {
+            return this.productDescription = value;
+        }
+
+        public int getPrice()
+        {
+            return this.productPrice;
+        }
+
+        public int setPrice(int value)
+        {
+            return this.productPrice = value;
+        }
+
+        public string getFullInfo()
+        {
+            return $"{this.productCode.ToString()}" + $" {this.productName}";
+        }
+
+        public int getFullPrice()
+        {
+            this.productPrice = (this.productPrice / 100) * 22;
+            return this.productPrice;
+        }
+    }
+        
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop.
+//Un prodotto è caratterizzato da:
+//-codice(numero intero)
+//- nome
+//- descrizione
+//- prezzo
+//- iva
+//Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+//Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interargirci per testare tutti i metodi che avete previsto.
+
+
+using csharp_oop_shop;
+
+Product product1 = new Product("Prova", "Prova descrizione", 10);
+
+Console.WriteLine(product1.getCode());
+Console.WriteLine(product1.getName());
+Console.WriteLine(product1.getDescription());
+Console.WriteLine(product1.getPrice());
+Console.WriteLine(product1.getFullInfo());
+Console.WriteLine(product1.getFullPrice());
+
+product1.setName("Prova set");
+product1.setDescription("Description set");
+product1.setPrice(11);
+
+Console.WriteLine(product1.getCode());
+Console.WriteLine(product1.getName());
+Console.WriteLine(product1.getDescription());
+Console.WriteLine(product1.getPrice());
+Console.WriteLine(product1.getFullInfo());
+Console.WriteLine(product1.getFullPrice());
